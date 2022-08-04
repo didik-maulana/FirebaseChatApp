72a11d754a7a3ae37d2d0b6243c0669080dcfca5
Didik Maulana Ardiansyah <didikmaulana49@gmail.com>
2022-08-04 14:57:05 +0700

    Getting started

---
+++ FirebaseChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChatApp
+//
+//  Created by Didik Maulana Ardiansyah on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
